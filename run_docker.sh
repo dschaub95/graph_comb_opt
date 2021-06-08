45b8e77a12d42277754505348ed9c1d5158be684
Darius Schaub <schaub.darius@gmail.com>
2021-06-08 12:01:18 +0200

    small docker file changes

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-nvidia-docker run -it -v $HOME/Desktop/TSP_code/graph_comb_opt/data:/home/S2V-DQN/data --name s2v-dqn-container s2v-dqn:test bash
+nvidia-docker run -it -v data:/home/S2V-DQN/data --name s2v-dqn-container s2v-dqn:test bash
